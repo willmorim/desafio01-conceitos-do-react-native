8ebe6536f6bce1ab3fd2ec4c8069b38e15e6bc42
willmorim <pdbawilliam@gmail.com>
2021-06-05 01:57:32 -0300

    Complete methods for the challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,38 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task if it's not empty
+    if(!newTaskTitle) return;
+
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('')
   }
 
   function handleMarkTaskAsDone(id: number) {
     //TODO - mark task as done if exists
+
+    const taskChecked = tasks.map(task => task.id === id ? {
+      ...task,
+      done: !task.done
+    } : task)
+
+    setTasks(taskChecked)
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    const removeTask = tasks.filter((task) => task.id !== id);
+    setTasks(removeTask)
   }
 
   return (
